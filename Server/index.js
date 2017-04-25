89631353bbe91221c73c86c669c1566c33078674
Jason Kuo <jasonkuo@Jasons-MacBook-Pro.local>
2017-04-25 14:49:58 -0700

    fixed issue... testing maxinstance

--- Server/index.js
+++ Server/index.js
@@ -20,17 +20,7 @@ app.get('/api/crawl/keywords/:keywords', async (req, res) => {
   var result = await Index.initiate();
   console.log('-----------result after awaiting for Index.initiate----------');
   res.send(JSON.stringify(result, null, 2));
-  // res.send(splitKeywords);
 })
-
-// async function testing(){
-//   var result = await Index.initiate();
-//   console.log('--------result------------\n', result);
-//   var result2 = JSON.stringify(result, null, 2);
-//   console.log('--------result2------------\n', result2);  
-// }
-
-// testing();
 
 var port = process.env.PORT || 5000;
 app.listen(port);
